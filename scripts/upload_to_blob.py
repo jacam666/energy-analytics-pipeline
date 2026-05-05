c07f968bf31862593edaf4d7cca15fce0502fbda
Jamie Cameron <ja6cam@gmail.com>
2026-05-05 14:30:09 +0100

    Add Python script to upload energy data to Azure Blob Storage

---
+++ scripts/upload_to_blob.py
@@ -0,0 +1,22 @@
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient
+
+STORAGE_ACCOUNT_NAME = "jclabstorage26"
+CONTAINER_NAME = "raw-data"
+LOCAL_FILE_PATH = "data/energy_transactions_2024_2026.csv"
+BLOB_NAME = "energy_transactions.csv"
+
+account_url = f"https://{STORAGE_ACCOUNT_NAME}.blob.core.windows.net"
+
+credential = DefaultAzureCredential()
+blob_service_client = BlobServiceClient(account_url, credential=credential)
+
+blob_client = blob_service_client.get_blob_client(
+    container=CONTAINER_NAME,
+    blob=BLOB_NAME
+)
+
+with open(LOCAL_FILE_PATH, "rb") as data:
+    blob_client.upload_blob(data, overwrite=True)
+
+print("Upload successful!")
